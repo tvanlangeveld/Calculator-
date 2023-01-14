0682b00c45bd20a84d9c13bde57a8133dbbcda5c
Tanner Vanlangeveld <t.vanlangeveld@outlook.com>
2023-01-14 15:27:13 -0700

    fixed syntax error

--- calc.js
+++ calc.js
@@ -1,3 +1,4 @@
+const { Console, clear } = require('console');
 const readline = require('readline');
 
 const reader = readline.createInterface({
@@ -13,9 +14,19 @@ reader.question("What would you like to calculate?", function(input){
 	const mathSymbol = tokens[0];
 	const num1 = Number(tokens[1]);
 	const num2 = Number(tokens[2]);
+// console.log('mathSymbol', mathSymbol);
+// console.log('num1', num1);
+// console.log('num2', num2);
+
+if (mathSymbol === "+"){console.log(num1 + num2)}
+
+if (mathSymbol === "-"){console.log(num1 - num2)}
+
+if (mathSymbol === "/"){console.log(num1 / num2)}
+	
+
 
 
 	// This line closes the connection to the command line interface.
 	reader.close()
-
 });
